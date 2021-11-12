ede868a648b1a2ef2fd1fce802dfaa4b0cf749b9
cesar <cesarc@nicasource.com>
2021-11-12 10:36:01 -0600

    First commit

---
+++ src/components/elements/Spinner.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import { StyledSpinner } from '../styles/StyledSpinner'
+
+// const StyledSpinner = styled.div`
+//   border: 5px solid #f3f3f3;
+//   border-top: 5px solid #16d47b;
+//   border-radius: 50%;
+//   width: 50px;
+//   height: 50px;
+//   animation: spin 0.8s linear infinite;
+//   margin: 20px auto;
+// `
+
+const Spinner = () => {
+  return <StyledSpinner />
+}
+
+export default Spinner
